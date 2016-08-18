8f622294033ee311853b1802051d4fec43dbcff5
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2016-08-18 21:24:46 +0200

    Removed another global variable.

--- Gas.rb
+++ Gas.rb
@@ -40,11 +40,11 @@ class GameWindow < Gosu::Window
 	
 	def restart  #### When you press Z, this method gets called
 		
-		$balls = []     ### Array containing every ball object.
+		@balls = []     ### Array containing every ball object.
     ball_radius = 11.0
 		
 		for i in 0..39  ### Repeat 40 times
-      $balls << Ball.new(self, rand_range(@univ_left + ball_radius, @univ_right - ball_radius), 
+      @balls << Ball.new(self, rand_range(@univ_left + ball_radius, @univ_right - ball_radius), 
         rand_range(@univ_bottom + ball_radius, @univ_top - ball_radius),
         rand(360), rand(5.0), ball_radius, 3.14*(ball_radius**2)) ### Create the ball
 		end
@@ -56,7 +56,7 @@ class GameWindow < Gosu::Window
 		
 		if @update_balls == true
 			## Update balls
-			$balls.each     { |inst|  inst.update }
+			@balls.each     { |inst|  inst.update }
 			## CHECK FOR BALL COLLISION. THIS IS DONE BY THE WINDOW, NOT BY EACH BALL. THE REASON IS OPTIMISATION.
 			self.check_ball_collision
 		end
@@ -69,7 +69,7 @@ class GameWindow < Gosu::Window
 			when Gosu::KbZ
 				self.restart
 			when Gosu::KbQ       ### When the game is paused, you can manually run each "step" of the simulation by pressing Q.
-				$balls.each     { |inst|  inst.update }
+				@balls.each     { |inst|  inst.update }
 				self.check_ball_collision
 			when Gosu::KbW
 				@update_balls = !@update_balls
@@ -77,9 +77,8 @@ class GameWindow < Gosu::Window
 	end
 	
 	def draw
-		
 		## Draw the balls
-		$balls.each     { |inst|  inst.draw }
+		@balls.each     { |inst|  inst.draw }
 		
 		### Draw the universe borders
 		draw_line(@univ_left,  @univ_bottom, 0xffffffff, @univ_right, @univ_bottom, 0xffffffff, 0)
@@ -90,27 +89,19 @@ class GameWindow < Gosu::Window
 		
 		### Draw the instructions
 		@font.draw("Press W to Unpause/Pause", (@univ_left+@univ_right)/2-100, @univ_bottom-60, 2)
-		@font.draw("Press Q to single-step", (@univ_left+@univ_right)/2-100, @univ_bottom-40, 2)
-		
+		@font.draw("Press Q to single-step",   (@univ_left+@univ_right)/2-100, @univ_bottom-40, 2)
+		@font.draw("Press Z to reset",         (@univ_left+@univ_right)/2-100, @univ_bottom-20, 2)
 	end
 	
 	def check_ball_collision  
-		
 		### This method has been optimised to only check each collision ONCE. Therefore the entire collision check is 2x faster.
 		### Thats also the reason why the method is run by the window, not by each ball.
 		
-		second_index = 1
-		
-		for i in 0..$balls.length-2  ## Ignore the last ball, since we have all the collisions checked by then
-			
-			for q in second_index..$balls.length-1  ### Check every ball from second_index
-				$balls[i].checkCollision($balls[q])
+		for i in 0..@balls.length-2  ## Ignore the last ball, since we have all the collisions checked by then
+			for q in i+1 .. @balls.length-1  ### Check every ball from second_index
+				@balls[i].checkCollision(@balls[q])
 			end
-			
-			second_index += 1
-			
 		end
-		
 	end
 	
 end
