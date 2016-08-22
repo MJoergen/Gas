7ee499efbeb913cd285baa693a5de16c3ee4b14c
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2016-08-22 06:45:31 +0200

    Added sound effect.

--- Ball.rb
+++ Ball.rb
@@ -1,110 +1,119 @@
 class Ball
-	
-	attr_reader :x, :y, :dir, :radius, :id, :mass, :vel_x, :vel_y
-	
-	def initialize(window, x, y, dir, vel, rad, mass)
-		@window, @x, @y, @dir, @radius, @mass = window, x, y, dir, rad, mass
-		
-		## Initial values.
-		@vel_x = Gosu::offset_x(@dir, vel)
-		@vel_y = Gosu::offset_y(@dir, vel)
-		
-		@colliding = false
-		@collision_point = false
-	end
-	
-	def update
-		@colliding = false
-		@collision_point = false
 
-		### Move the ball
-		@x = @x + @vel_x
-		@y = @y + @vel_y
-		
-		### Check collision with walls
-		if @x+@radius > @window.univ_right  and @vel_x > 0
-			@vel_x = -@vel_x
-		end
-    
-		if @x-@radius < @window.univ_left   and @vel_x < 0
-			@vel_x = -@vel_x
-		end
-		
-		if @y+@radius > @window.univ_top    and @vel_y > 0
-			@vel_y = -@vel_y
-		end
-		
-		if @y-@radius < @window.univ_bottom and @vel_y < 0
-			@vel_y = -@vel_y
-		end
-	end
-	
-	def draw
-    color = 0xffFF0000  # Red
+  attr_reader :x, :y, :dir, :radius, :id, :mass, :vel_x, :vel_y, :color
+
+  def initialize(window, x, y, dir, vel, rad, color, sound)
+    @window, @x, @y, @dir, @radius, @mass, @color, @sound = window, x, y, dir, rad, 3.14*rad*rad, color, sound
+
+    ## Initial values.
+    @vel_x = Gosu::offset_x(@dir, vel)
+    @vel_y = Gosu::offset_y(@dir, vel)
+
+    @colliding = false
+    @collision_point = false
+  end
+
+  def update
+    @colliding = false
+    @collision_point = false
+
+    ### Move the ball
+    @x = @x + @vel_x
+    @y = @y + @vel_y
+
+    ### Check collision with walls
+    if @x+@radius > @window.univ_right  and @vel_x > 0
+      @vel_x = -@vel_x
+    end
+
+    if @x-@radius < @window.univ_left   and @vel_x < 0
+      @vel_x = -@vel_x
+    end
+
+    if @y+@radius > @window.univ_top    and @vel_y > 0
+      @vel_y = -@vel_y
+    end
+
+    if @y-@radius < @window.univ_bottom and @vel_y < 0
+      @vel_y = -@vel_y
+    end
+  end
+
+  def draw
+    color = @color  # Red
     if @colliding == true
-      color = 0xff00FF00  # Green
+      color = @color | 0x00808080
     end
     @window.circle_img.draw_rot(@x, @y, 0, @dir, 0.5, 0.5, @radius/50.0, @radius/50.0, color)
-		
-		if @collision_point == true
-			@window.circle_img.draw_rot(@collisionPointX, @collisionPointY, 1, @dir,
-        0.5, 0.5, 7.0/50.0, 7.0/50.0, 0xff0000FF)
-		end
-	end
-	
-	def checkCollision(inst)  ### This method is only called once for each pair of balls
-		if @x + @radius + inst.radius > inst.x and
-		   @x < inst.x + @radius + inst.radius and
-		   @y + @radius + inst.radius > inst.y and
-		   @y < inst.y + @radius + inst.radius      ## This big 'if' is an optimization.
-			
-			dist = Gosu::distance(inst.x, inst.y, @x, @y)
-			if dist < (@radius + inst.radius)
-				
-				@collisionPointX = ((@x * inst.radius) + (inst.x * @radius))/(@radius + inst.radius)
-				@collisionPointY = ((@y * inst.radius) + (inst.y * @radius))/(@radius + inst.radius)
-				
-				@collision_point = true
-				
-				new_vel_self = new_velocity(@mass, inst.mass, Vector[@vel_x, @vel_y], Vector[inst.vel_x, inst.vel_y],
-          Vector[@x, @y], Vector[inst.x, inst.y])
-				new_vel_inst = new_velocity(inst.mass, @mass, Vector[inst.vel_x, inst.vel_y], Vector[@vel_x, @vel_y],
-          Vector[inst.x, inst.y], Vector[@x, @y])
-				
-				self.collision_ball(new_vel_self)
-				inst.collision_ball(new_vel_inst)
-				
-			end
-		end
-	end
-	
-	def collision_ball(new_vel)
-		@colliding = true
-		
-		@vel_x = new_vel[0]
-		@vel_y = new_vel[1]
-	end
-	
+
+    if @collision_point == true
+      @window.circle_img.draw_rot(@collisionPointX, @collisionPointY, 1, @dir,
+				  0.5, 0.5, 7.0/50.0, 7.0/50.0, color ^ 0x00FFFFFF)
+    end
+  end
+
+  def checkCollision(inst)  ### This method is only called once for each pair of balls
+    if @x + @radius + inst.radius > inst.x and
+      @x < inst.x + @radius + inst.radius and
+      @y + @radius + inst.radius > inst.y and
+      @y < inst.y + @radius + inst.radius      ## This big 'if' is an optimization.
+
+      dist = Gosu::distance(inst.x, inst.y, @x, @y)
+      if dist < (@radius + inst.radius)
+
+	@collisionPointX = ((@x * inst.radius) + (inst.x * @radius))/(@radius + inst.radius)
+	@collisionPointY = ((@y * inst.radius) + (inst.y * @radius))/(@radius + inst.radius)
+
+	@collision_point = true
+
+	new_vel_self = new_velocity(@mass, inst.mass, Vector[@vel_x, @vel_y], Vector[inst.vel_x, inst.vel_y],
+				    Vector[@x, @y], Vector[inst.x, inst.y])
+	new_vel_inst = new_velocity(inst.mass, @mass, Vector[inst.vel_x, inst.vel_y], Vector[@vel_x, @vel_y],
+				    Vector[inst.x, inst.y], Vector[@x, @y])
+
+	self.collision_ball(new_vel_self)
+	inst.collision_ball(new_vel_inst)
+
+      end
+    end
+  end
+
+  def collision_ball(new_vel)
+    @colliding = true
+
+    delta_x = @vel_x - new_vel[0]
+    delta_y = @vel_y - new_vel[1]
+
+    if @sound == true
+      delta_v = Math.sqrt(delta_x**2 + delta_y**2)
+      volume = delta_v / 7.0
+      @window.hit_sound.play(volume)
+    end
+
+    @vel_x = new_vel[0]
+    @vel_y = new_vel[1]
+  end
+
   ## This values the new velocity after the collision
   ## m1, v1, c1 : Mass, velocity, and center for this object.
   ## m2, v2, c2 : Mass, velocity, and center for the other object.
   ## The calculations are taken from the last equation of
   ## The velocity and centers must be Vector's.
   ## https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional_collision_with_two_moving_objects
-	def new_velocity(m1, m2, v1, v2, c1, c2)
-		f = (2*m2)/(m1+m2)  ## Number
-		
-		dv = v1 - v2  ### Vector
-		dc = c1 - c2  ### Vector
-		
-		v_new = v1 - f * (dv.inner_product(dc))/(dc.inner_product(dc)) * dc  ### Vector
-		
+  def new_velocity(m1, m2, v1, v2, c1, c2)
+    f = (2*m2)/(m1+m2)  ## Number
+
+    dv = v1 - v2  ### Vector
+    dc = c1 - c2  ### Vector
+
+    v_new = v1 - f * (dv.inner_product(dc))/(dc.inner_product(dc)) * dc  ### Vector
+
     ## If the two objects are already moving away from each, then don't change the velocity.
-		if dv.inner_product(dc) > 0
-			return v1     ### Vector
-		else
-			return v_new  ### Vector
-		end
-	end
-	
+    if dv.inner_product(dc) > 0
+      return v1     ### Vector
+    else
+      return v_new  ### Vector
+    end
+  end
+
 end
--- Gas.rb
+++ Gas.rb
@@ -3,103 +3,115 @@ require 'matrix'
 require_relative 'Ball'
 
 class GameWindow < Gosu::Window
-	
-	WIDTH  = 860
-	HEIGHT = 540
-	
-	attr_reader :circle_img
+
+  WIDTH  = 1060
+  HEIGHT = 740
+
+  attr_reader :circle_img
   attr_reader :univ_left, :univ_right, :univ_top, :univ_bottom
-  
-	def initialize
-		super(WIDTH, HEIGHT, false)
-		
+  attr_reader :hit_sound
+
+  def initialize
+    super(WIDTH, HEIGHT, false)
+
     @univ_left   = 30
     @univ_right  = WIDTH - 30
     @univ_bottom = 70
     @univ_top    = HEIGHT - 70
-		
-		## Only two images are used in this program
-		@point_img  = Gosu::Image.new(self, "media/Point2.png", true)
-		@circle_img = Gosu::Image.new(self, "media/filled_circle.png", true)
-		
-		## Default font
-		@font = Gosu::Font.new(self, Gosu::default_font_name, 16)
-		
-		## Game is paused by default. Unpause by pressing W
-		@update_balls = false
 
-		## Creates the objects
-		self.restart
-	end
-  
+    ## Only two images are used in this program
+    @point_img  = Gosu::Image.new(self, "media/Point2.png", true)
+    @circle_img = Gosu::Image.new(self, "media/filled_circle.png", true)
+
+    ## Default font
+    @font = Gosu::Font.new(self, Gosu::default_font_name, 16)
+
+    ## Sound when balls collide
+    @hit_sound = Gosu::Sample.new(self, "media/hit_sound.wav")
+
+    ## Game is paused by default. Unpause by pressing W
+    @update_balls = false
+
+    ## Creates the objects
+    self.restart
+  end
+
   def rand_range(low, high)
     return rand(high-low) + low
   end
-	
-	def restart  #### When you press Z, this method gets called
-		@balls = []     ### Array containing every ball object.
+
+  def restart  #### When you press Z, this method gets called
+    @balls = []     ### Array containing every ball object.
     ball_radius = 9.0
-		
-		for i in 0..79  ### Repeat 40 times
+
+    for i in 0..19  ### Repeat 40 times
       @balls << Ball.new(self, rand_range(@univ_left + ball_radius, @univ_right - ball_radius), 
-        rand_range(@univ_bottom + ball_radius, @univ_top - ball_radius),
-        rand(360), rand(8.0), ball_radius, 3.14*(ball_radius**2)) ### Create the ball
-		end
-	end
-	
-	def update
-		self.caption = "Gas  -  [FPS: #{Gosu::fps.to_s}]"
-		
-		if @update_balls == true
-			## Update balls
-			@balls.each     { |inst|  inst.update }
-			## CHECK FOR BALL COLLISION. THIS IS DONE BY THE WINDOW, NOT BY EACH BALL. THE REASON IS OPTIMISATION.
-			self.check_ball_collision
-		end
-	end
-	
-	def button_down(id)
-		case id
-			when Gosu::KbEscape
-				close
-			when Gosu::KbZ
-				self.restart
-			when Gosu::KbQ       ### When the game is paused, you can manually run each "step" of the simulation by pressing Q.
-				@balls.each     { |inst|  inst.update }
-				self.check_ball_collision
-			when Gosu::KbW
-				@update_balls = !@update_balls
-		end
-	end
-	
-	def draw
-		## Draw the balls
-		@balls.each     { |inst|  inst.draw }
-		
-		### Draw the universe borders
-		draw_line(@univ_left,  @univ_bottom, 0xffffffff, @univ_right, @univ_bottom, 0xffffffff, 0)
-		draw_line(@univ_left,  @univ_top,    0xffffffff, @univ_right, @univ_top,    0xffffffff, 0)
-		
-		draw_line(@univ_left,  @univ_bottom, 0xffffffff, @univ_left,  @univ_top,    0xffffffff, 0)
-		draw_line(@univ_right, @univ_bottom, 0xffffffff, @univ_right, @univ_top,    0xffffffff, 0)
-		
-		### Draw the instructions
-		@font.draw("Press W to Unpause/Pause", (@univ_left+@univ_right)/2-100, @univ_bottom-60, 2)
-		@font.draw("Press Q to single-step",   (@univ_left+@univ_right)/2-100, @univ_bottom-40, 2)
-		@font.draw("Press Z to reset",         (@univ_left+@univ_right)/2-100, @univ_bottom-20, 2)
-	end
-	
-	def check_ball_collision  
-		### This method has been optimised to only check each collision ONCE. Therefore the entire collision check is 2x faster.
-		### Thats also the reason why the method is run by the window, not by each ball.
-		
-		for i in 0..@balls.length-2  ## Ignore the last ball, since we have all the collisions checked by then
-			for q in i+1 .. @balls.length-1  ### Check every ball from second_index
-				@balls[i].checkCollision(@balls[q])
-			end
-		end
-	end
-	
+			 rand_range(@univ_bottom + ball_radius, @univ_top - ball_radius),
+			 rand(360), rand(8.0), ball_radius, 0xFF_FF0000, false) 
+    end
+    for i in 0..79  ### Repeat 40 times
+      @balls << Ball.new(self, rand_range(@univ_left + ball_radius, @univ_right - ball_radius), 
+			 rand_range(@univ_bottom + ball_radius, @univ_top - ball_radius),
+			 rand(360), rand(8.0), ball_radius, 0xFF_0000FF, false) 
+    end
+    @balls << Ball.new(self, rand_range(@univ_left + ball_radius, @univ_right - ball_radius), 
+		       rand_range(@univ_bottom + ball_radius, @univ_top - ball_radius),
+		       rand(360), rand(8.0), ball_radius*1.4 , 0xFF_FFFFFF, true) 
+  end
+
+  def update
+    self.caption = "Gas  -  [FPS: #{Gosu::fps.to_s}]"
+
+    if @update_balls == true
+      ## Update balls
+      @balls.each     { |inst|  inst.update }
+      ## CHECK FOR BALL COLLISION. THIS IS DONE BY THE WINDOW, NOT BY EACH BALL. THE REASON IS OPTIMISATION.
+      self.check_ball_collision
+    end
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::KbEscape
+      close
+    when Gosu::KbZ
+      self.restart
+    when Gosu::KbQ       ### When the game is paused, you can manually run each "step" of the simulation by pressing Q.
+      @balls.each     { |inst|  inst.update }
+      self.check_ball_collision
+    when Gosu::KbW
+      @update_balls = !@update_balls
+    end
+  end
+
+  def draw
+    ## Draw the balls
+    @balls.each     { |inst|  inst.draw }
+
+    ### Draw the universe borders
+    draw_line(@univ_left,  @univ_bottom, 0xffffffff, @univ_right, @univ_bottom, 0xffffffff, 0)
+    draw_line(@univ_left,  @univ_top,    0xffffffff, @univ_right, @univ_top,    0xffffffff, 0)
+
+    draw_line(@univ_left,  @univ_bottom, 0xffffffff, @univ_left,  @univ_top,    0xffffffff, 0)
+    draw_line(@univ_right, @univ_bottom, 0xffffffff, @univ_right, @univ_top,    0xffffffff, 0)
+
+    ### Draw the instructions
+    @font.draw("Press W to Unpause/Pause", (@univ_left+@univ_right)/2-100, @univ_bottom-60, 2)
+    @font.draw("Press Q to single-step",   (@univ_left+@univ_right)/2-100, @univ_bottom-40, 2)
+    @font.draw("Press Z to reset",         (@univ_left+@univ_right)/2-100, @univ_bottom-20, 2)
+  end
+
+  def check_ball_collision  
+    ### This method has been optimised to only check each collision ONCE. Therefore the entire collision check is 2x faster.
+    ### Thats also the reason why the method is run by the window, not by each ball.
+
+    for i in 0..@balls.length-2  ## Ignore the last ball, since we have all the collisions checked by then
+      for q in i+1 .. @balls.length-1  ### Check every ball from second_index
+	@balls[i].checkCollision(@balls[q])
+      end
+    end
+  end
+
 end
 
 # show the window
